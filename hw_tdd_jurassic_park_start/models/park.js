853c64b1596a28a26c12093ea21d0fdb8dbe3045
Stefano Binando <sbinando@gmail.com>
2021-11-23 16:29:13 +0000

    park tests 4 and 5 --- ok

--- hw_tdd_jurassic_park_start/models/park.js
+++ hw_tdd_jurassic_park_start/models/park.js
@@ -2,10 +2,21 @@ const Park = function(name, price){
     this.name = name;
     this.price = price;
     this.dinosaurs = [];
+
 }
 
 Park.prototype.addDino = function(dino){
     this.dinosaurs.push(dino);
 }
+
+
+Park.prototype.removeDino = function(){
+    this.dinosaurs.shift();
+}
+
+Park.prototype.mostVisitors = function(){
+
+}
+
 
 module.exports = Park;
